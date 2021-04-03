7920f2c8c20cc0be6a21b25d9e3b066e71388eea
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2021-04-04 00:22:53 +0600

    Added microservices with api-gateway

--- course-app/index.js
+++ course-app/index.js
@@ -1,21 +1,20 @@
 const app = require("express")();
 const appid = process.env.APPID;
 
-app.get("/user", (req,res) => 
-res.send(`appid: ${appid} user app: says hello!`))
+app.get("/course", (req,res) => 
+res.send(`appid: ${appid} course app: says hello!`))
 
-app.get("/user/app1", (req,res) => 
+app.get("/course/app1", (req,res) => 
 res.send(`appid: ${appid} app1 page: says hello!`))
  
-app.get("/user/app2", (req,res) => 
+app.get("/course/app2", (req,res) => 
 res.send(`appid: ${appid} app2 page: says hello!`))
  
-app.get("/user/admin", (req,res) => 
+app.get("/course/admin", (req,res) => 
 res.send(`appid: ${appid} ADMIN page: very few people should see this`))
 
-
 app.all('*', (req, res) => {
-	res.send('Unauthorized route - user-service');
+	res.send('Unauthorized route - course-service');
 	// res.send('Unauthorized route');
 });
 
